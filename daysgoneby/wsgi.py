ad9915d953e284f99b6f4e76ab9bd029e1910230
Ryan Rubin <ryanmrubin@gmail.com>
2015-12-04 08:58:55 -0600

    collect settings into settings dir, labelled per environment

--- daysgoneby/wsgi.py
+++ daysgoneby/wsgi.py
@@ -3,6 +3,6 @@ from django.core.wsgi import get_wsgi_application
 from dj_static import Cling
 
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "daysgoneby.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "daysgoneby.settings.dev")
 
 application = Cling(get_wsgi_application())
