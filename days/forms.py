1937083897f01a29a69192f03370b4efe2c54da4
Ryan Rubin <ryanmrubin@gmail.com>
2015-12-08 23:35:15 -0600

    Throw error if return date is not after departure date

--- days/forms.py
+++ days/forms.py
@@ -1,7 +1,15 @@
 from django import forms
+from django.core.exceptions import ValidationError
 
 
 class DaysGoneCalculatorForm(forms.Form):
     departure_date = forms.DateField()
     return_date = forms.DateField()
+
+    def clean(self):
+        cleaned_data = super(DaysGoneCalculatorForm, self).clean()
+        departure_date = cleaned_data['departure_date']
+        return_date = cleaned_data['return_date']
+        if not departure_date < return_date:
+            raise ValidationError("Please enter a departure date prior to the return date.")
 
