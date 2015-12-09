f1560e76bb1ac099bf6f2ee7563801121b1a5a3f
Ryan Rubin <ryanmrubin@gmail.com>
2015-12-08 23:46:01 -0600

    show more data in results

--- days/views.py
+++ days/views.py
@@ -5,12 +5,15 @@ from .forms import DaysGoneCalculatorForm
 
 def calculate_days_gone(request):
     form = DaysGoneCalculatorForm()
-    result = 0
+    result_data = {}
     if request.method == "POST":
         form = DaysGoneCalculatorForm(request.POST)
         if form.is_valid():
             departure_date = form.cleaned_data['departure_date']
             return_date = form.cleaned_data['return_date']
-            result = (return_date - departure_date).days - 1 # neither travel day counts
+            result_data['days_gone'] = (return_date - departure_date).days - 1 # neither travel day counts
 
-    return render(request, 'days/days_gone_calculator.html', dict(form=form, result=result))
+            result_data['departure_date'] = departure_date
+            result_data['return_date'] = return_date
+
+    return render(request, 'days/days_gone_calculator.html', dict(form=form, result_data=result_data))
