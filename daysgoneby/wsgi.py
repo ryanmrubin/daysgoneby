bc595d2e5511fc9b2afc765afab3642fd30c981b
Ryan Rubin <ryanmrubin@gmail.com>
2016-03-11 21:29:47 -0600

    use whitenoise to serve static content

--- daysgoneby/wsgi.py
+++ daysgoneby/wsgi.py
@@ -1,8 +1,10 @@
 import os
 from django.core.wsgi import get_wsgi_application
 from dj_static import Cling
+from whitenoise.django import DjangoWhiteNoise
 
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "daysgoneby.settings.dev")
 
 application = Cling(get_wsgi_application())
+application = DjangoWhiteNoise(application)
