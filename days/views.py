dfd10df69f0e6b3e05085b14b51e6336367febe6
Ryan Rubin <ryanmrubin@gmail.com>
2016-03-14 21:39:39 -0500

    remove notes to self

--- days/views.py
+++ days/views.py
@@ -64,15 +64,3 @@ def n400_date_entry(request):
                'trips_so_far': trips_so_far,
                'total_days_gone': total_days_gone}
     return render(request,'days/n400_date_entry.html', context)
-
-    # redirect similar urls to the N-400/etc urls.
-    # highlight days greater than 180 as needing further explanation
-    # compare total days gone to the max number (if present for 913 OR MORE in 5 years prior to n-400 date, to they meet the req.)--colornumber and display note
-
-    # add messages app so as to alert when some of the following happen
-    # sort trips in trips_so_far
-    # overlapping trips mean throw an error and tell to fix.
-    # throw out days out of the country if they were more than 5 years prior to n-400 date
-        # show whole trip but include a note. (put notes column in table--the 180+ shoulddo that too.)
-        # in this event, show extra line--Total Days Gone in Statutory Period
-    # if trip range includes date of n-400 submission, throw error saying to check date of submission
