e0f96b8030f753c4e93bd851e5e1736a6bed1cab
Ryan Rubin <ryanmrubin@gmail.com>
2016-03-14 21:39:12 -0500

    redirect mistyped urls to correct url

--- days/urls.py
+++ days/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import url
+from django.views.generic.base import RedirectView
 from . import views
 
 
@@ -6,4 +7,6 @@ urlpatterns = [
     url(r'^$', views.calculate_days_gone),
     url(r'^N-400/$', views.n400_home, name='n400_home'),
     url(r'^N-400/dates/$', views.n400_date_entry, name='n400_date_entry'),
+    url(r'(?i)^n-?400/$', RedirectView.as_view(pattern_name='n400_home', permanent=True)),
+    url(r'(?i)^n-?400/dates/$', RedirectView.as_view(pattern_name='n400_date_entry', permanent=True)),
 ]
